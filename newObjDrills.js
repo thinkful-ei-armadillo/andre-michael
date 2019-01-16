852d870364aab52d6ca36ec239a9eed1e735518b
Andre K W <akw.ndre@gmail.com>
2019-01-16 12:23:20 -0500

    iterate over object

--- newObjDrills.js
+++ newObjDrills.js
@@ -1,3 +1,6 @@
+/* eslint-disable indent */
+'use strict';
+
 let loaf = {flour: 300, water: 210, hydration: function() {
     return ((this.water / this.flour) * 100);
     }
@@ -6,3 +9,17 @@ let loaf = {flour: 300, water: 210, hydration: function() {
 console.log(`${loaf.flour}, ${loaf.water}`);
 console.log(loaf.hydration());
 
+
+
+const myObj = {
+    foo: 'hey',
+    bar: 25,
+    fum: 100,
+    quux: 'whats up',
+    spam: 0,
+};
+
+let key;
+for(key in myObj) {
+    console.log(`${key}: ${myObj[key]}`);
+}
