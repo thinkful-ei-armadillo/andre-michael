6a9dd648aea0b1bd8e64bff43841950cd875b7fd
Michael Bramble <mike.w.bramble@gmail.com>
2019-01-16 11:17:39 -0500

    added drills1

---
+++ drills1.js
@@ -0,0 +1,19 @@
+function createMyObject(){
+    return {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function(){
+            return 'hello';
+        },
+    };
+}
+
+function updateObject(obj){
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
+}
+
